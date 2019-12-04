6709ca1dfca36a55c74b5e8c604174e5d0c8d16c
BeM835 <turejskijleonid004@gmail.com>
2019-12-04 12:12:14 +0800

    Revert to 1.0

--- app/src/main/java/bem/ware/yoipis/MainActivity.java
+++ app/src/main/java/bem/ware/yoipis/MainActivity.java
@@ -1,70 +1,44 @@
-/**
- * Simple ip checker by BM835
- *
- * https://github.com/BM835
- * https://bm835.github.io/ip_checker
- */
 package bem.ware.yoipis;
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
-import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.lang.Void;
 
 public class MainActivity extends AppCompatActivity {
         TextView outs;
-        TextView locOut;
         View view;
         Snackbar waitforip;
         getIP getip;
-        String locIP;
 
 
     @Override
         protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         outs = findViewById(R.id.textView);
-        locOut = findViewById(R.id.textView2);
         view = findViewById(R.id.fab);
         getip = new getIP();
 
         /**ip will gets in start */
         getip.execute();
-        getLocalIP();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,48 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
                 getip.execute();
                 /**if you don't understand it will will ask ip on line
-                126 via AsyncTask */
-                getLocalIP();
+                72 via AsyncTask */
             }
         });
-        locOut.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("label", locIP);
-                clipboard.setPrimaryClip(clip);
-
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Copied!",
-                        Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // получим идентификатор выбранного пункта меню
-        int id = item.getItemId();
-
-        TextView infoTextView = (TextView) findViewById(R.id.textView);
-
-        // Операции для выбранного пункта меню
-        switch (id) {
-            case R.id.action_settings:
-                Intent about_ = new Intent(MainActivity.this, AboutActivity.class);
-                startActivity(about_);
-                return true;
-            case R.id.action_kill:
-                crash();
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
     /**
@@ -150,9 +85,8 @@ public class MainActivity extends AppCompatActivity {
         /** check and output your ip */
         @Override
         protected Void doInBackground(Void... params) {
-            String err = "Success";
             try {
-                URL yahoo = new URL("https://us-central1-ip-checker-7ecce.cloudfunctions.net/getIP");
+                URL yahoo = new URL("https://api.ipify.org");
                 URLConnection yc = yahoo.openConnection();
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(
@@ -166,21 +100,10 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 final String TAG = "NoAnyIP";
                 Log.wtf(TAG, e.toString());
-                try {
-        int timeoutMs = 1500;
-        Socket sock = new Socket();
-        SocketAddress sockaddr = new InetSocketAddress("8.8.8.8", 53);
-
-        sock.connect(sockaddr, timeoutMs);
-        sock.close();
-
-        err = e.toString();
-    } catch (IOException ee) { err = getResources().getString(R.string.noinetcon); }
-}
                 //waitforip.dismiss();
-                waitforip.make(view, err, Snackbar.LENGTH_SHORT)
-                        .setAction("Retry", null).show();
-            
+                waitforip.make(view, e.toString(), Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+            }
             return null;
         }
 
@@ -190,8 +113,6 @@ public class MainActivity extends AppCompatActivity {
             outs.setText(ipis);
 
             vvait.setVisibility(ProgressBar.INVISIBLE);
-
-            pleaseRUOGP();
         }
 
         /*@RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -200,66 +121,4 @@ public class MainActivity extends AppCompatActivity {
         }*/
     }
 
-    public void copy(View v){
-            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-            ClipData clip = ClipData.newPlainText("label", outs.getText().toString());
-            clipboard.setPrimaryClip(clip);
-
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    "Copied!",
-                    Toast.LENGTH_SHORT);
-            toast.show();
-
-            pleaseRUOGP();
-        }
-
-
-
-    public void getLocalIP(){
-        try {
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface intf : interfaces) {
-                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
-                        locIP = addr.getHostAddress();
-                        locOut.setText("Local ip is: " + locIP);
-                    }
-                }
-            }
-        } catch (Exception e) {e.printStackTrace();} // for now eat exceptions
-    }
-
-    public void crash(){
-        int a = 5/0;
-    }
-
-    //RUOGP = Rate us on google play
-    public void pleaseRUOGP(){
-        final String APP_PREFERENCES = "prefs";
-        final String APP_PREFERENCES_RUOGP = "isRate";
-        SharedPreferences mSettings;
-        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-        int ir = 0;
-
-        if(mSettings.contains(APP_PREFERENCES_RUOGP)) {
-            ir = mSettings.getInt(APP_PREFERENCES_RUOGP, 0);
-        }
-
-        if (ir != 2) {
-            Random random = new Random();
-            int istry = random.nextInt(2);
-            String istryy = String.valueOf(istry);
-            Log.i("rand", istryy);
-            if(istry>=1){
-                Log.i("rand", "going to show");
-               FragmentManager manager = getSupportFragmentManager();
-                RateUs rus = new RateUs();
-               rus.show(manager, "dialog");
-            }
-        }
-        //}
-    }
 }
-
-
--- readme.md
+++ readme.md
@@ -1,12 +1,9 @@
-[![Build Status](https://travis-ci.com/BM835/ip_checker.svg?branch=master)](https://travis-ci.org/BM835/ip_checker)[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)[![Release](https://img.shields.io/github/release/bm835/ip_checker.svg?style=flat)](https://github.com/bm835/ip_checker/releases)![Repo size](https://img.shields.io/github/repo-size/bm835/ip_checker.svg?style=flat)
-
-# [IP checker](https://play.google.com/store/apps/details?id=bem.ware.yoipis&utm_source=gh&utm_campaign=readme&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1)
+# [IP checker](app/release/app-release.apk)
 ![icon](images/ico.png)
 
-[![Get it on Google Play](https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png)](https://play.google.com/store/apps/details?id=bem.ware.yoipis&utm_source=gh&utm_campaign=readme&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1)
+My IP checker checking your IP in [ipyfy web servise](https://www.ipify.org/).
 
 Screenshots
 ------------------------
 
-![screenshot](images/scr2.png)
-![screenshot2](images/scr1.png)
+![screenshot](images/scr.png)
